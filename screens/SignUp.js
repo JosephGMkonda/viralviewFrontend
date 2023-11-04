e8da379994704dad5e619fc4ff42ad4cfed9e316
sangwaniKondowe <josephgmkonda@gmail.com>
2023-11-04 23:22:20 +0400

    first commmit

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import {View,Text} from 'react-native'
+import {View,Text,SafeAreaView} from 'react-native'
 import { SIZES,COLORS } from '../constants/theme'
 
 const SignUp =() => {
   return (
 
-    <SafeAreaView>
+    <SafeAreaView>  
     <View>
         <View 
         style={{
