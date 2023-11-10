4cf631ce0e16a85db954959d5deed2e4ae51fd49
sangwaniKondowe <josephgmkonda@gmail.com>
2023-11-10 23:15:34 +0400

    =========

--- navigation/BottomTabNavigation.js
+++ navigation/BottomTabNavigation.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import {feather,Ionicons,FontAwesome,FontAwesome5} from '@expo/vector-icons'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
-import { Platform, View,LinearGradient } from 'react-native'
+import { Feather, Ionicons, FontAwesome, FontAwesome5 } from '@expo/vector-icons'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Platform, View, } from 'react-native'
 import { COLORS } from '../constants/theme'
 import Feed from '../screens/Feed'
 import Chat from '../screens/Chat'
 import Create from '../screens/Create'
 import Notification from '../screens/Notification'
 import Profile from '../screens/Profile'
+import { LinearGradient } from 'expo-linear-gradient'
 
 
 const Tab = createBottomTabNavigator()
@@ -27,115 +28,91 @@ const screenOptions = {
         backgroundColor: COLORS.white,
         borderTopLeftRadius: 20,
         borderTopRightRadius: 20,
-        
-
-
-
     }
 }
 
 const BottomTabNavigation = () => {
 
-  return (
+    return (
 
-    <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Navigator screenOptions={screenOptions}>
 
-        <Tab.Screen
-        name='Feed'
-        component={Feed}
-        options={{
-            tabBarIcon: ({focused}) => {
-                return(
-                    <feather name="home" size={24} color={focused ? COLORS.primary : COLORS.blue}/>
-                )
-            }
-        }}
-        >
+            <Tab.Screen
+                name='Feed'
+                component={Feed}
+                options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <Feather name="home" size={24} color={focused ? COLORS.primary : COLORS.blue} />
+                        )
+                    }
+                }}
+            />
 
-        </Tab.Screen>
+            <Tab.Screen
+                name='Chat'
+                component={Chat}
+                options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <Ionicons name="chatbox-outline" size={24} color={focused ? COLORS.primary : COLORS.blue} />
+                        )
+                    }
+                }}
+            />
 
+            <Tab.Screen
+                name='Create'
+                component={Create}
+                options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <LinearGradient
+                                colors={['#F68464', '#EEA849']}
+                                style={{
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    width: Platform.OS == 'ios' ? 50 : 60,
+                                    height: Platform.OS == 'ios' ? 50 : 60,
+                                    top: Platform.OS == 'ios' ? -10 : -20,
+                                    borderRadius: 22,
+                                    borderColor: '#fff',
+                                    borderWidth: 4
+                                }}
+                            >
+                                <Feather name="plus-circle" size={24} color={COLORS.white} />
+                            </LinearGradient>
+                        )
+                    }
+                }}
+            />
 
-        
-        <Tab.Screen
-        name='Chat'
-        component={Chat}
-        options={{
-            tabBarIcon: ({focused}) => {
-                return(
-                    <feather name="chatbox-outline" size={24} color={focused ? COLORS.primary : COLORS.blue}/>
-                )
-            }
-        }}
-        >
+            <Tab.Screen
+                name='Notifications'
+                component={Notification}
+                options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <FontAwesome name="heart" size={24} color={focused ? COLORS.primary : COLORS.blue} />
+                        )
+                    }
+                }}
+            />
 
-        </Tab.Screen>
+            <Tab.Screen
+                name='Profile'
+                component={Profile}
+                options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <FontAwesome name="user-circle" size={24} color={focused ? COLORS.primary : COLORS.blue} />
+                        )
+                    }
+                }}
+            />
 
-        
-        <Tab.Screen
-        name='Create'
-        component={Create}
-        options={{
-            tabBarIcon: ({focused}) => {
-                return(
-
-                    <LinearGradient
-                    colors={['#F68464','#EEA849']}
-                    style={{
-                        alignItems: "center",
-                        justifyContent: "center",
-                        width: Platform.OS =='ios' ? 50: 60,
-                        height: Platform.OS =='ios' ? 50: 60,
-                        top: Platform.OS == 'ios' ? -10 : -20,
-                        borderRadius: 22,
-                        borderColor: '#fff',
-                        borderWidth: 4
-
-                    }}>
-                     
-                        <feather name="plus-circle" size={24} color={COLORS.white}/>
-
-                    </LinearGradient>
-                    
-                )
-            }
-        }}
-        />
-
-
-    <Tab.Screen
-        name='Notifications'
-        component={Notification}
-        options={{
-            tabBarIcon: ({focused}) => {
-                return(
-                    <FontAwesome name="heart" size={24} color={focused ? COLORS.primary : COLORS.blue}/>
-                )
-            }
-        }}
-        />
-
-    <Tab.Screen
-        name='Profile'
-        component={Profile}
-        options={{
-            tabBarIcon: ({focused}) => {
-                return(
-                    <FontAwesome name="user-circle" size={24} color={focused ? COLORS.primary : COLORS.blue}/>
-                )
-            }
-        }}
-        />
-
-        
-
-        
-
-    
-
-        
-
-    </Tab.Navigator >
-  )
+        </Tab.Navigator >
+    )
 }
 
 export default BottomTabNavigation
