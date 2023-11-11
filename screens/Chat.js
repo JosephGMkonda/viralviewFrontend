ea9137d2978a1ebd9269008a5d5394d17c94524d
sangwaniKondowe <josephgmkonda@gmail.com>
2023-11-11 20:01:23 +0400

    ......

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,6 +1,6 @@
 
 
-import {View,Text} from 'react'
+import {View,Text} from 'react-native'
 
 export const Chat = () =>{
   return (
--- screens/Create.js
+++ screens/Create.js
@@ -1,4 +1,4 @@
-import {View,Text} from 'react'
+import {View,Text} from 'react-native'
 
 export const  Create =() => {
   return (
